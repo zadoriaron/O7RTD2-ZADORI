731f909646fb714e939511a7200fab0e90360558
zadoriaron <789zadori.aron@gmail.com>
2025-05-10 22:14:27 +0200

    backend now returns id at getall method

--- BACKEND/FragranceWebshop_Entities/Dtos/PerfumDto/PerfumViewDto.cs
+++ BACKEND/FragranceWebshop_Entities/Dtos/PerfumDto/PerfumViewDto.cs
@@ -8,6 +8,7 @@ namespace FragranceWebshop_Entities.Dtos.PerfumDto
 {
     public class PerfumViewDto
     {
+        public string PerfumId { get; set; }
         public string PerfumName {  get; set; }
         public string RecommendedSeason { get; set; }
         public int Price { get; set; }
---
+++ FRONTEND/src/app/perfum.ts
@@ -0,0 +1,11 @@
+export class Perfum {
+
+    PerfumId:string = ""
+    perfumName:string = "";
+    recommendedSeason:string = "";
+    price:number = 0;
+    purchaseCount = 0;
+
+    constructor()
+
+}
